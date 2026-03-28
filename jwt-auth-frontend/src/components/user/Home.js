511890e0466aedf82625c1f2d9b4fdb39d0fea0a
Katherine Mae Guzman <kathewinnnn@gmail.com>
2026-03-28 14:04:14 +0800

    k

--- jwt-auth-frontend/src/components/user/Home.js
+++ jwt-auth-frontend/src/components/user/Home.js
@@ -552,7 +552,7 @@ const styles = `
   .ch-nav-cta { width: 35%; padding: 12px; margin-left: 15.6px; }
 }
 
-@media (max-width: 750px) and (min-width: 428px) {
+@media (max-width: 750px) and (min-width: 414px) {
   .ch-nav-cta { width: 50%; padding: 12px; margin-left: 15.6px; }
 }
 `;
